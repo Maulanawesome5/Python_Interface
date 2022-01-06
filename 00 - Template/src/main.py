4db868287af88c3e919a20ec3f4b87d8541a7741
Maulanawesome5 <maulanawesome5@gmail.com>
2022-01-06 18:56:15 +0700

    Mengubah isi main.py

--- 00 - Template/src/main.py
+++ 00 - Template/src/main.py
@@ -1,3 +1,10 @@
-print("Selamat untuk saya")
-print("Instalasi python di Visual Studio")
-print("Berhasil..!!")
+import tkinter # Packge untuk membuat tampilan GUI
+
+# Toplevel widget of Tk which represents mostly the main window of an 
+# application. It has an associated Tcl interpreter.
+main_window = tkinter.Tk()
+
+
+
+# mainloop() adalah method untuk menampilkan jendela aplikasi
+main_window.mainloop()
