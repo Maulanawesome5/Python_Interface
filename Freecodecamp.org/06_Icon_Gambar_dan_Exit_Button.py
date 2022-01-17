ab84c38c27a5ed10cbced732cefd2ff24401f220
Maulanawesome5 <maulanawesome5@gmail.com>
2022-01-18 00:16:45 +0700

    Berhasil memasukkan gambar jpg, dan resize gambar

--- Freecodecamp.org/06_Icon_Gambar_dan_Exit_Button.py
+++ Freecodecamp.org/06_Icon_Gambar_dan_Exit_Button.py
@@ -1,4 +1,5 @@
 from tkinter import *
+from PIL import ImageTk, Image # Modul untuk memasukkan gambar
 
 root = Tk() # Top level widget
 root.title('Tutorial from freecodecamp.org')
@@ -6,11 +7,22 @@ root.iconbitmap('D:/LATIHAN PEMROGRAMAN/PYTHON_GUI/Freecodecamp.org/icon/bitcoin
 # Method iconbitmap() hanya mendukung gambar/icon dengan format .ico
 # kalau bisa harus desain dulu, atau download dari internet
 
+# Memasukkan foto ke dalam aplikasi
+resolusi_foto = (500, 500) # Tipe data tuple, untuk mengubah ukuran foto jika kebesaran
+
+my_img = ImageTk.PhotoImage(
+    Image.open(
+        'D:/LATIHAN PEMROGRAMAN/PYTHON_GUI/Freecodecamp.org/icon/tampan1.jpg'
+    ).resize(resolusi_foto) # Karena ukuran asli foto besar, maka digunakan method resize()
+)
+
+my_label = Label(image=my_img)
+my_label.pack()
+# my_label.grid(row=0, column=0)
+
 # Membuat tombol untuk exit aplikasi
-button_quit = Button(master=root, text="Exit", command=root.quit)
+button_quit = Button(master=root, text="Pencet untuk keluar", command=root.quit)
 button_quit.pack()
-
-
-
+# button_quit.grid(row=0, column=1)
 
 root.mainloop()
