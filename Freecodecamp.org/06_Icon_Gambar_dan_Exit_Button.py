02dee1e193bb7410e0a12d09744cd4b5a0c5cdef
Maulanawesome5 <maulanawesome5@gmail.com>
2022-01-17 23:39:40 +0700

    Berhasil menambahkan icon untuk tampilan GUI

--- Freecodecamp.org/06_Icon_Gambar_dan_Exit_Button.py
+++ Freecodecamp.org/06_Icon_Gambar_dan_Exit_Button.py
@@ -2,6 +2,11 @@ from tkinter import *
 
 root = Tk() # Top level widget
 root.title('Tutorial from freecodecamp.org')
+root.iconbitmap('D:/LATIHAN PEMROGRAMAN/PYTHON_GUI/Freecodecamp.org/icon/bitcoin.ico')
+# Method iconbitmap() hanya mendukung gambar/icon dengan format .ico
+# kalau bisa harus desain dulu, atau download dari internet
+
+
 
 
 
