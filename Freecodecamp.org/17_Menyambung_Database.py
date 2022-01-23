42e46b5305b016e1a0937076fb0b96a768290e6b
Maulanawesome5 <maulanawesome5@gmail.com>
2022-01-23 13:28:12 +0700

    Membuat database dengan python sqlite3

---
+++ Freecodecamp.org/17_Menyambung_Database.py
@@ -0,0 +1,33 @@
+from tkinter import *
+from PIL import ImageTk, Image
+import sqlite3
+
+root = Tk()
+root.title('Tutorial from freecodecamp.org')
+root.iconbitmap('D:/LATIHAN PEMROGRAMAN/PYTHON_GUI/Freecodecamp.org/icon/bitcoin.ico')
+root.geometry('400x400')
+
+# Database
+# Create a database or connect to one
+conn = sqlite3.connect('address_book.db')
+
+# Membuat cursor
+c = conn.cursor()
+
+# Membuat table
+c.execute("""CREATE TABLE address (
+    first_name text,
+    last_name text,
+    address text,
+    city text,
+    state text,
+    zipcode integer
+)""")
+
+# commit change
+conn.commit()
+
+# close connection
+conn.close()
+
+root.mainloop()
