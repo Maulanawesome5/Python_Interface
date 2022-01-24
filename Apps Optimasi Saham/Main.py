1888cae42d52a4c76e1c769dac10d291514013df
Maulanawesome5 <maulanawesome5@gmail.com>
2022-01-24 17:53:29 +0700

    Membuat Main.py untuk tampilan GUI

---
+++ Apps Optimasi Saham/Main.py
@@ -0,0 +1,15 @@
+from tkinter import *
+
+
+## Membuat top level widget
+root = Tk()
+root.title('Optimasi Portofolio Saham')
+root.iconbitmap('D:/LATIHAN PEMROGRAMAN/PYTHON_GUI/Apps Optimasi Saham/icon/business-color_stock_icon-icons.com_53431.ico')
+
+
+
+
+
+
+# Method untuk memanggil aplikasi
+root.mainloop()
