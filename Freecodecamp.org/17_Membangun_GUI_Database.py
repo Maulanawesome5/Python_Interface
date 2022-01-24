c4971011b37dfb1d0acd847b3abbe91b6fd98ce3
Maulanawesome5 <maulanawesome5@gmail.com>
2022-01-25 00:11:54 +0700

    17_Membangun_GUI gagal, tidak bisa menambah data

--- Freecodecamp.org/17_Membangun_GUI_Database.py
+++ Freecodecamp.org/17_Membangun_GUI_Database.py
@@ -7,30 +7,36 @@ root.title('Tutorial from freecodecamp.org')
 root.iconbitmap('D:/LATIHAN PEMROGRAMAN/PYTHON_GUI/Freecodecamp.org/icon/bitcoin.ico')
 root.geometry('400x400')
 
+# Database
+# Membuat database baru atau menyambung ke database yang ada
+conn = sqlite3.connect('address_book.db')
+
+# Membuat cursor
+c = conn.cursor()
 
 # create submit function for database
 def submit():
-    # Database
-    # Create a database or connect to one
+    # Membuat database baru atau menyambung ke database yang ada
     conn = sqlite3.connect('address_book.db')
 
     # Membuat cursor
     c = conn.cursor()
 
-    # Membuat table
-    # c.execute("""CREATE TABLE address (
+    # # Membuat table
+    # # Tipe data yang terdapat pada SQLite3 = text, integer, real (desimal), null, blob (image, audio, video)
+    # c.execute("""CREATE TABLE addresses (
     #     first_name text,
     #     last_name text,
     #     address text,
     #     city text,
     #     state text,
     #     zipcode integer
-    # )""")
+    #     )
+    # """)
 
     # Insert into table
-    c.execute(
-        "INSERT INTO address VALUES (:f_name, :l_name, :address, :city, :state, :zipcode)",
-        {
+    c.execute("""INSERT INTO addresses VALUES (:f_name, :l_name, :address, :city, :state, :zipcode)""",
+        {   #key    : value
             'f_name': f_name.get(),
             'l_name': l_name.get(),
             'address': address.get(),
@@ -40,7 +46,8 @@ def submit():
         }
     )
 
-    # Clear the textbox
+    # Clear the textbox, setiap selesai menambah data
+    # tulisan di textbox akan otomatis terhapus
     f_name.delete(0, END)
     l_name.delete(0, END)
     address.delete(0, END)
@@ -48,8 +55,8 @@ def submit():
     state.delete(0, END)
     zipcode.delete(0, END)
 
-    conn.commit() # commit change to database
-    conn.close() # close connection from database
+    conn.commit() # commit setiap perubahan
+    conn.close() # close connection dari database setelah selesai
 
 
 
@@ -82,27 +89,29 @@ def query():
 # Membuat textbox
 f_name = Entry(root, width=30).grid(row=0, column=1, padx=20)
 l_name = Entry(root, width=30).grid(row=1, column=1)
-addr = Entry(root, width=30).grid(row=2, column=1)
+address = Entry(root, width=30).grid(row=2, column=1)
 city = Entry(root, width=30).grid(row=3, column=1)
-states = Entry(root, width=30).grid(row=4, column=1)
+state = Entry(root, width=30).grid(row=4, column=1)
 zipcode = Entry(root, width=30).grid(row=5, column=1)
 
 # Membuat textbox label
 f_name_label = Label(master=root, text="First Name").grid(row=0, column=0)
 l_name_label = Label(master=root, text="Last Name").grid(row=1, column=0)
-addr_label = Label(master=root, text="Address").grid(row=2, column=0)
+address_label = Label(master=root, text="Address").grid(row=2, column=0)
 city_label = Label(master=root, text="City").grid(row=3, column=0)
-states_label = Label(master=root, text="States").grid(row=4, column=0)
+state_label = Label(master=root, text="State").grid(row=4, column=0)
 zipcode_label = Label(master=root, text="Zipcode").grid(row=5, column=0)
 
-# Create submit button
+
+# Membuat submit button untuk menambahkan data dari GUI ke database
 submit_btn = Button(
     master=root,
     text="Add record to database",
     command=submit
 ).grid(row=6, column=0, columnspan=2, pady=10, ipadx=100)
 
-# Create query button
+
+# Membuat query button, untuk menampilkan isi database ke terminal
 query_btn = Button(
     master=root,
     text="Show records",
@@ -112,5 +121,7 @@ query_btn = Button(
 
 
 
+conn.commit() # commit setiap perubahan
+conn.close() # close connection dari database setelah selesai
 
 root.mainloop()
